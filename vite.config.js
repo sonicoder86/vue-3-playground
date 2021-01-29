ef14178e423c81b1de96af92719aec72da2f1726
blacksonic <soos.gabor86@gmail.com>
2021-01-29 17:43:29 +0100

    fix warning with webcomponents

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default {
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => /^x-/.test(tag)
+        }
+      }
+    }),
+    vueJsx()
+  ],
   base: './',
-  vueCompilerOptions: {
-    isCustomElement: tag => /^x-/.test(tag)
-  }
 };
