0d5faf5307edef7aeae52d7efb98cacaa15fc7bf
blacksonic <soos.gabor86@gmail.com>
2021-01-30 20:28:09 +0100

    fix axios headers

--- axios.js
+++ axios.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-axios.get('https://localhost:3000')
+axios.get('http://localhost:3000', { headers: { 'Accept': 'text/html, text/plain, application/json' } })
   .then((response) => {
     console.log(response)
   })
