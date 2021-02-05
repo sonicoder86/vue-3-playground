9510fc0bdf9fb6729701c5636f05f0b363bf21d1
blacksonic <soos.gabor86@gmail.com>
2021-02-05 20:59:49 +0100

    update sfc type

--- src/typescript/sfc.d.ts
+++ src/typescript/sfc.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
-  import { defineComponent } from 'vue';
-  const component: ReturnType<typeof defineComponent>;
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
   export default component;
 }
