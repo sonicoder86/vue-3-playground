9d7f4ba3c43e4ce1475353d661cb66d1ce83f4e4
blacksonic <soos.gabor86@gmail.com>
2021-02-22 08:55:39 +0100

    refactor e2e puppeteer -> cypress

--- e2e/typescript/index.test.ts
+++ e2e/typescript/index.test.ts
@@ -1,11 +1,6 @@
-import 'expect-puppeteer';
-
 describe('Vue 3 Playground', () => {
-  beforeAll(async () => {
-    await page.goto('http://localhost:3000')
-  });
-
-  it('should display playground text on page', async () => {
-    await expect(page).toMatch('Vue 3.0.5 Playground');
+  it('should display playground text on page', () => {
+    cy.visit('/');
+    cy.contains('h2', 'Vue 3.0.5 Playground');
   });
 });
