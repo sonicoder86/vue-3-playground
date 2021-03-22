8c94831b3ed5070b30f2d544851d44b51ebc3826
blacksonic <soos.gabor86@gmail.com>
2021-03-22 19:22:14 +0100

    upgrade deps

--- src/javascript/components/Header.vue
+++ src/javascript/components/Header.vue
@@ -19,7 +19,7 @@ export default defineComponent({
 
 <template>
 <div class="py-5 text-center">
-  <img class="d-block mx-auto mb-4" src="../../../public/logo.png" alt="" width="72" height="72">
+  <img class="d-block mx-auto mb-4" src="/logo.png" alt="" width="72" height="72">
   <h2>Vue {{ version }} Playground</h2>
   <p class="lead">
     Below is an example form built with the Vue {{ version }} Composition API.
