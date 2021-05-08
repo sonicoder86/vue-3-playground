b6bc2c1cdeacbe5b24a8ab3b8ae3c329e2576e6d
blacksonic <soos.gabor86@gmail.com>
2021-05-08 11:42:12 +0200

    update dependencies and fix api

--- src/javascript/components/Exchange.vue
+++ src/javascript/components/Exchange.vue
@@ -3,7 +3,8 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   async setup() {
-    const response = await fetch('https://api.exchangeratesapi.io/latest');
+    const url = 'http://data.fixer.io/api/latest?access_key=534719332efb7559a8f331ef73491776&format=1';
+    const response = await fetch(url);
     const exchangeRates = await response.json();
 
     return { rates: exchangeRates.rates };
