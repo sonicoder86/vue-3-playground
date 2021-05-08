c8eac67277a129beb1c41ff7b970589d5a48c5e6
blacksonic <soos.gabor86@gmail.com>
2021-05-08 11:58:36 +0200

    fix e2e test

--- e2e/javascript/index.test.js
+++ e2e/javascript/index.test.js
@@ -1,6 +1,6 @@
 describe('Vue 3 Playground', () => {
   it('should display playground text on page', () => {
     cy.visit('/');
-    cy.contains('h2', 'Vue 3.0.5 Playground');
+    cy.contains('h2', 'Vue 3.0.6 Playground');
   });
 });
